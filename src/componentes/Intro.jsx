0a8b563263e8d41c6191f8b239c37eb6e25b3957
Minseok Kim^C <ksj5147@gmail.com^C>
2024-09-28 14:53:27 -0700

    intro

--- src/componentes/Intro.jsx
+++ src/componentes/Intro.jsx
@@ -1,7 +1,42 @@
 import React from "react"
+import about from "../assets/image/about.jpg"
 
 const Intro = () => {
-  return <div></div>
+  return (
+    <section id="intro">
+      <div class="intro__inner">
+        <h1 class="intro__title">MINSEOK's PORTFOLIO</h1>
+        <div class="intro__lines" aria-hidden="true">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+        <div class="intro__text">
+          <div class="text">
+            <div>PORTFOLIO</div>
+            <div> </div>
+            <div>by MINSEOK</div>
+          </div>
+          <div class="img">
+            <img src={about} alt="어바웃" />
+          </div>
+        </div>
+        <div class="intro__lines bottom" aria-hidden="true">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Intro
