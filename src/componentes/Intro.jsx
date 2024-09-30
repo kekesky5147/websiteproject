b53f9f1a31972f4a37755b6447e0b75c6ccb9c1e
Minseok Kim^C <ksj5147@gmail.com^C>
2024-09-30 13:58:39 -0700

    intro

--- src/componentes/Intro.jsx
+++ src/componentes/Intro.jsx
@@ -1,38 +1,43 @@
 import React from "react"
 import about from "../assets/image/about.jpg"
 
+const introText = {
+  title: "MINSEOK's PORTFOLIO",
+  desc: ["PORTFOLIO", "by MINSEOK"],
+}
+
 const Intro = () => {
   return (
     <section id="intro">
-      <div class="intro__inner">
-        <h1 class="intro__title">MINSEOK's PORTFOLIO</h1>
-        <div class="intro__lines" aria-hidden="true">
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
+      <div className="intro__inner">
+        <h1 className="intro__title">MINSEOK's PORTFOLIO</h1>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
         </div>
-        <div class="intro__text">
-          <div class="text">
-            <div>PORTFOLIO</div>
-            <div> </div>
-            <div>by MINSEOK</div>
+        <div className="intro__text">
+          <div className="text">
+            <div>{introText.desc[0]}</div>
+            <div>{introText.desc[1]}</div>
+            <div></div>
           </div>
-          <div class="img">
+          <div className="img">
             <img src={about} alt="어바웃" />
           </div>
         </div>
-        <div class="intro__lines bottom" aria-hidden="true">
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
         </div>
       </div>
     </section>
