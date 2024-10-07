d9a5b5828a60e6575a349a8af3facfc569374145
Minseok Kim^C <ksj5147@gmail.com^C>
2024-10-07 13:55:45 -0700

    site

--- src/componentes/Site.jsx
+++ src/componentes/Site.jsx
@@ -17,8 +17,7 @@ const Site = () => {
             </div>
             <h3 className="tite">웹 표준을 준수한 사이트 제작</h3>
             <div className="btn">
-              <a href="https://github.com/webstoryboy/port2023-vite">code</a>
-              <a href="https://port2023-vite.netlify.app/">view</a>
+              <a href="https://github.com/kekesky5147/websiteproject">code</a>
             </div>
             <div className="info">
               <span>site coding</span>
@@ -30,18 +29,17 @@ const Site = () => {
             <span className="num">2.</span>
             <div className="text">
               <div>Make</div>
-              <div>Coin market app</div>
+              <div>Cryptoplace app</div>
               <div>react.js</div>
             </div>
             <h3 className="title"> Make website from React</h3>
             <div className="btn">
-              <a href="https://github.com/webstoryboy/port2023-react">code</a>
-              <a href="https://port2023-react.netlify.app/">view</a>
+              <a href="https://github.com/kekesky5147/cryptoplace">code</a>
             </div>
             <div className="info">
               <span>site coding</span>
-              <span>production period : two days</span>
-              <span>use stack : HTML5/CSS3, CSS Variable, Vite</span>
+              <span>production period : 2 weeks</span>
+              <span>use stack : React, CSS Variable</span>
             </div>
           </acrticle>
           <article className="site__item s3">
@@ -51,15 +49,14 @@ const Site = () => {
               <div>Weather app with</div>
               <div>React.js</div>
             </div>
-            <h3 className="title">Make website by vue</h3>
+            <h3 className="title">Make website from React</h3>
             <div className="btn">
-              <a href="https://github.com/webstoryboy/port2023-vue">code</a>
-              <a href="https://port2023-vue.netlify.app/">view</a>
+              <a href="https://github.com/kekesky5147/weather-app">code</a>
             </div>
             <div className="info">
               <span>site coding</span>
               <span>production period : 2weeks</span>
-              <span>use stack : vue.js CSS Variable, vite</span>
+              <span>use stack : React, CSS Variable, vite</span>
             </div>
           </article>
           <acrticle className="site__item s4">
@@ -69,10 +66,9 @@ const Site = () => {
               <div>Todo list app</div>
               <div>React.js</div>
             </div>
-            <h3>Make website by Next</h3>
+            <h3>Make website from React</h3>
             <div className="btn">
-              <a href="https://github.com/webstoryboy/port2023-next">code</a>
-              <a href="https://port2023-next.netlify.app/">view</a>
+              <a href="https://github.com/kekesky5147/todo-app">code</a>
             </div>
             <div className="info">
               <span>site coding</span>
